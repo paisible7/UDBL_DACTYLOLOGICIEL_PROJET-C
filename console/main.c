42a89ebf0697d355d387386847052df182d8f085
paisible7 <22km127@esisalama.org>
2024-06-08 06:59:01 +0200

    COMMIT8JUIN = lancement du jeu

--- console/main.c
+++ console/main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    sleep(2);
+    printf("\n\n*****   *****   UDBL - DACTYLOGICIEL *****   *****\n\n");
+    sleep(4);
+    printf("\nKALONJI MWINSHI AUGUSTIN\n");
+    sleep(1);
+    printf("\nKANGAJ AKAWAW GLORIA\n");
+    sleep(1);
+    printf("\nKAPINGA MBAYA PATRICIA\n");
+    sleep(1);
+    printf("\nKANYEBA TSHIMBALANGA DEBORAH\n");
+    sleep(1);
     return 0;
 }
