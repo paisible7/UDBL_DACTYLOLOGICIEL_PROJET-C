12939471b0b32147aa39b851db0b7391bc38b655
paisible7 <22km127@esisalama.org>
2024-06-08 07:14:47 +0200

    COMMIT8JUIN = creation la fonction start

--- console/main.c
+++ console/main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "start.h"
 
 int main()
 {
-    sleep(2);
-    printf("\n\n*****   *****   UDBL - DACTYLOGICIEL *****   *****\n\n");
-    sleep(4);
-    printf("\nKALONJI MWINSHI AUGUSTIN\n");
-    sleep(1);
-    printf("\nKANGAJ AKAWAW GLORIA\n");
-    sleep(1);
-    printf("\nKAPINGA MBAYA PATRICIA\n");
-    sleep(1);
-    printf("\nKANYEBA TSHIMBALANGA DEBORAH\n");
-    sleep(1);
+    start();
     return 0;
 }
