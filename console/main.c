0ef3198912bff27a5c3c854d131e6e1591ebe10d
deborah <kanyebad41@gmail.com>
2024-06-13 15:07:22 +0200

    PARIE sur l'aide

--- console/main.c
+++ console/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+void afficherMenuAide()
+{
+    printf("Menu aide\n");
 
+    printf("Description de l'application:
+     une application de dactylogiciel c'est un logiciel d'aide a l'amelioration de la saisie rapide au clavier\n"
+     );
+     printf("version du jeu: 2.0\n");
+
+     printf("concrepteurs:Deborah , Patricia , Gloria , Augustin\n");
+     printf("Date de conception : 10 mai 2024\n");
+     printf("Autres information: cette application a été developpée en c avec l'intrface graphique.\n");
+}
 int main()
 {
-    printf("Hello world!\n");
+    // Appel de la fonction pour afficher le menu d'aide
+
+    afficherMenuAide();
+    
     return 0;
 }
