cfce7ab7fa83c868b9622bf218d2c4715993b47d
paisible7 <22km127@esisalama.org>
2024-06-20 00:09:36 +0200

    COMMIT20JUIN = amélioration apportée à la version 1.0 pour la version 1.5

--- console/main.c
+++ console/main.c
@@ -3,103 +3,109 @@
 #include <time.h>
 #include <string.h>
 
-typedef struct
-{
+// Macros pour les codes d'échappement ANSI qui va permettre de mettre la couleur
+#define ANSI_COLOR_RESET "\033[0m"
+#define ANSI_COLOR_GREEN "\033[0;32m"
+#define ANSI_COLOR_RED "\033[0;31m"
+#define ANSI_COLOR_YELLOW "\033[0;33m"
+#define ANSI_COLOR_BLUE "\033[0;55m"
+#define ANSI_COLOR_CYAN "\033[0;36m"
+
+typedef struct {
     char nom[50];
     double temps;
 } Joueur;
 
-void start()
-{
+void start() {
     sleep(1);
-    printf("\n\n*****   *****   UDBL - DACTYLOGICIEL *****   *****\n\n");
+    printf("\n\n\t\t" ANSI_COLOR_BLUE "*****\t*****\tUDBL - DACTYLOGICIEL\t*****\t*****\n\n" ANSI_COLOR_RESET);
     sleep(2);
-    printf("\nKALONJI MWINSHI AUGUSTIN\n");
-    sleep(1);
-    printf("\nKANGAJ AKAWAW GLORIA\n");
-    sleep(1);
-    printf("\nKAPINGA MBAYA PATRICIA\n");
-    sleep(1);
-    printf("\nKANYEBA TSHIMBALANGA DEBORAH\n");
-    sleep(4);
+
+    const char* noms[] = {
+        "KALONJI MWINSHI AUGUSTIN\n\t\t\t>>\t@m.paisible7",
+        "KANGAJ AKAWAW GLORIA",
+        "KAPINGA MBAYA PATRICIA",
+        "KANYEBA TSHIMBALANGA DEBORAH"
+    };
+
+    for (int i = 0; i < 4; i++) { // une boucle qui va permettre d'afficher les noms des participants chaque 2 secondes
+        printf("\n\t\t\t" ANSI_COLOR_GREEN "%s\n" ANSI_COLOR_RESET, noms[i]);
+        printf("\t\t\t============================\n" ANSI_COLOR_RESET);
+        sleep(2);
+    }
+
+    sleep(2);
     system("clear");
 }
 
-void afficherAide()
-{
-    FILE* aide = NULL;
-    int txt = 0;
-    aide = fopen("aide.txt", "r");
-    if (aide == NULL)
-    {
-        printf("Erreur lors de l'ouverture du fichier d'aide.\n");
+void afficherAide() {
+    FILE* aide = fopen("aide.txt", "r");//fonction qui ouvre le fichier aide.txt pour affiche l'aide
+    if (aide == NULL) {
+        printf(ANSI_COLOR_RED "\t\t\tErreur lors de l'ouverture du fichier d'aide.\n" ANSI_COLOR_RESET);
         return;
     }
 
-    while ((txt = fgetc(aide)) != EOF)
-    {
-        printf("%c", txt);
+    int txt;
+    while ((txt = fgetc(aide)) != EOF) {//pour detecter la fin de,ligne du fichier >> End Of File
+        putchar(txt);
     }
     printf("\n");
     fclose(aide);
 }
 
-void configurer()
-{
-    int nombre_mots, i;
+void configurer() {
+    int nombre_mots;
     char mot[100];
-    FILE* fichier = fopen("mots.txt", "w");
+    FILE* fichier = fopen("phrases.txt", "a");
 
-    if (fichier == NULL)
-    {
-        printf("Erreur lors de l'ouverture du fichier pour la configuration.\n");
+    if (fichier == NULL) {
+        printf(ANSI_COLOR_RED "\t\t\tErreur lors de l'ouverture du fichier pour la configuration.\n" ANSI_COLOR_RESET);
         return;
     }
 
-    printf("Combien de mots voulez-vous entrer ? ");
+    printf("\t\t\tCombien de mots voulez-vous entrer ? ");
     scanf("%d", &nombre_mots);
     getchar(); // Pour supprimer le retour à la ligne laissé par scanf
 
-    for (i = 0; i < nombre_mots; i++)
-    {
-        printf("Entrez le mot %d : ", i + 1);
+    for (int i = 0; i < nombre_mots; i++) {
+        printf("\n\t\t\tEntrez le mot %d : ", i + 1);
         fgets(mot, sizeof(mot), stdin);
         fprintf(fichier, "%s", mot);
     }
 
     fclose(fichier);
-    printf("Les phrases ont été enregistrés dans le fichier 'phrases.txt'.\n");
+    if(nombre_mots == 1){
+        printf(ANSI_COLOR_GREEN "\n\t\t\tLa phrase a été enregistrée avec succès !.\n" ANSI_COLOR_RESET);
+
+    }
+    printf(ANSI_COLOR_GREEN "\n\t\t\tLes phrases ont été enregistrées avec succès !.\n" ANSI_COLOR_RESET);
 }
 
-int jeu()
-{
-    char** mots = NULL; // Tableau de pointeurs pour les mots
+int jeu() {
+    char** mots = NULL;
     int nb_mots = 0;
     char input[100];
-    int nombre_joueurs, i, j;
+    int nombre_joueurs;
     Joueur temp;
 
-    // Lecture des mots depuis le fichier
-    FILE* fichier = fopen("phrases.txt", "r");
-    if (fichier == NULL)
-    {
-        printf("Erreur : impossible de charger les phrases.\n");
+
+    FILE* fichier = fopen("phrases.txt", "r");// Lecture des mots depuis le fichier des phrases
+    if (fichier == NULL) {
+        printf(ANSI_COLOR_RED "\t\t\tErreur : impossible de charger les phrases.\n" ANSI_COLOR_RESET);
         return 1;
     }
 
-    // Allocation dynamique pour le tableau de pointeurs
+
     mots = malloc(100 * sizeof(char*));
-    if (mots == NULL)
-    {
-        printf("Erreur d'allocation mémoire pour les mots.\n");
+    if (mots == NULL) {
+        printf(ANSI_COLOR_RED "\t\t\tErreur d'allocation mémoire pour les mots.\n" ANSI_COLOR_RESET);
         fclose(fichier);
         return 1;
     }
 
-    // Lecture des mots et stockage dans le tableau
-    char buffer[100];
-    while (fgets(buffer, sizeof(buffer), fichier))
-    {
+
+    char buffer[100];// Lecture des mots et stockage dans le tableau
+    while (fgets(buffer, sizeof(buffer), fichier)) {
         buffer[strcspn(buffer, "\n")] = '\0';
         mots[nb_mots] = malloc((strlen(buffer) + 1) * sizeof(char));
         strcpy(mots[nb_mots], buffer);
@@ -107,75 +113,67 @@ int jeu()
     }
     fclose(fichier);
 
-    printf("Entrez le nombre de joueurs : ");
+
+    printf("\n\t\t\tEntrez le nombre de joueurs : ");
     scanf("%d", &nombre_joueurs);
-    getchar(); // pour supprimer le retour à la ligne
+    getchar(); // Pour supprimer le retour à la ligne
 
     Joueur joueurs[nombre_joueurs];
 
-    for (i = 0; i < nombre_joueurs; i++)
-    {
-        printf("Entrez le nom du joueur %d : ", i + 1);
+    for (int i = 0; i < nombre_joueurs; i++) {
+        // Saisie du nom du joueur
+        printf("\n\t\t\tEntrez le nom du joueur %d : ", i + 1);
         fgets(joueurs[i].nom, sizeof(joueurs[i].nom), stdin);
-        joueurs[i].nom[strcspn(joueurs[i].nom, "\n")] = '\0'; // Enlever le retour àla ligne à la fin du nom
+        joueurs[i].nom[strcspn(joueurs[i].nom, "\n")] = '\0'; // Enlever le retour à la ligne à la fin du nom
 
-        printf("Prêt ? Allez !\n");
+        printf("\n\t\t\tPrêt ? Allez !\n");
 
-        // Sélection aléatoire d'une phrase non encore utilisé
-        srand(time(NULL)); // generateur des nombres aléatoires
+        // Sélection aléatoire d'une phrase non encore utilisée
+        srand(time(NULL)); // Initialisation du générateur de nombres aléatoires
         int index_mot;
-        do
-        {
-            index_mot = rand() % nb_mots; // selection aleatoire
-        }
-        while (mots[index_mot] == NULL);   // Verification du phrase si déà utilisé ou pas
+        do {
+            index_mot = rand() % nb_mots; // Sélection aléatoire
+        } while (mots[index_mot] == NULL); // Vérification si la phrase a déjà été utilisée
 
-        printf("Tapez le mot suivant : %s\n", mots[index_mot]);
+        printf("\t\t\tTapez le mot suivant : %s%s%s\n\n\t\t\t", ANSI_COLOR_GREEN, mots[index_mot], ANSI_COLOR_RESET);
 
         time_t debut, fin;
         double temps_saisie;
-        time(&debut); // début du chronomètre
+        time(&debut); // Début du chronomètre
 
-        do
-        {
-            fgets(input, sizeof(input), stdin); // saisie de l'utilisateur
-            input[strcspn(input, "\n")] = '\0'; // supprimé le retour à la ligne à la fin de la phrases
+        // Saisie et vérification de la phrase
+        do {
+            fgets(input, sizeof(input), stdin); // Saisie de l'utilisateur
+            input[strcspn(input, "\n")] = '\0'; // Suppression du retour à la ligne à la fin de la phrase
 
-            if (strcmp(input, mots[index_mot]) != 0)
-            {
-                printf("Désolé, vous n'avez pas tapé la phrases correctement. Réessayez : ");
+            if (strcmp(input, mots[index_mot]) != 0) {
+                printf(ANSI_COLOR_RED "\t\t\tDésolé, vous n'avez pas tapé la phrase correctement.\n\n\t\t\t"ANSI_COLOR_YELLOW"Réessayez : " ANSI_COLOR_RESET);
             }
-        }
-        while (strcmp(input, mots[index_mot]) != 0);
+        } while (strcmp(input, mots[index_mot]) != 0);
 
-        time(&fin); // fin du chronomètre
+        time(&fin); // Fin du chronomètre
 
         temps_saisie = difftime(fin, debut);
         joueurs[i].temps = temps_saisie;
-        printf("Bravo %s ! Vous avez tapé la phrase en %.2f secondes.\n", joueurs[i].nom, joueurs[i].temps);
+        printf(ANSI_COLOR_GREEN "\t\t\tBravo %s ! Vous avez tapé la phrase en %.2f secondes.\n" ANSI_COLOR_RESET, joueurs[i].nom, joueurs[i].temps);
 
         // Marquer le mot comme utilisé en le libérant
         free(mots[index_mot]);
         mots[index_mot] = NULL;
     }
 
-    // Libérer la mémoire allouée pour les mots
-    for (i = 0; i < nb_mots; i++)
-    {
-        if (mots[i] != NULL)
-        {
+    // Libération de la mémoire allouée pour les mots
+    for (int i = 0; i < nb_mots; i++) {
+        if (mots[i] != NULL) {
             free(mots[i]);
         }
     }
     free(mots);
 
     // Tri des joueurs par temps en utilisant le tri à bulles
-    for (i = 0; i < nombre_joueurs - 1; i++)
-    {
-        for (j = 0; j < nombre_joueurs - i - 1; j++)
-        {
-            if (joueurs[j].temps > joueurs[j + 1].temps)
-            {
+    for (int i = 0; i < nombre_joueurs - 1; i++) {
+        for (int j = 0; j < nombre_joueurs - i - 1; j++) {
+            if (joueurs[j].temps > joueurs[j + 1].temps) {
                 temp = joueurs[j];
                 joueurs[j] = joueurs[j + 1];
                 joueurs[j + 1] = temp;
@@ -184,75 +182,73 @@ int jeu()
     }
 
     // Affichage des scores des joueurs en ordre croissant de temps
-    printf("\nScores :\n");
-    for (i = 0; i < nombre_joueurs; i++)
-    {
-        printf("%s : %.2f secondes\n", joueurs[i].nom, joueurs[i].temps);
+    printf("\n" ANSI_COLOR_YELLOW "\t\t\tScores :\n" ANSI_COLOR_RESET);
+    for (int i = 0; i < nombre_joueurs; i++) {
+        printf(ANSI_COLOR_YELLOW "\t\t\t%s : %.2f secondes\n" ANSI_COLOR_RESET, joueurs[i].nom, joueurs[i].temps);
     }
 
     return 0;
 }
 
-int main()
-{
+int main() {
     int choix;
     start();
 
-    do
-    {
-        printf("\n\t===========================\n");
-        printf("\t     MENU PRINCIPAL\n");
-        printf("\t===========================\n\n");
-        printf("\n\t\t1. Démarrer\n");
-        printf("\n\t\t2. Configurer\n");
-        printf("\n\t\t3. Aide\n");
-        printf("\n\t\t0. Quitter\n");
-        printf("\n\tSélectionner une option : ");
+    do {
+        // Affichage du menu principal
+        printf("\n\t\t\t" ANSI_COLOR_CYAN "===========================\n");
+        printf("\t\t\t||\tMENU PRINCIPAL \t ||\n");
+        printf("\t\t\t===========================\n\n" ANSI_COLOR_RESET);
+        printf("\n\t\t\t||\t1. Démarrer\t ||\n");
+        printf("\n\t\t\t||\t2. Configurer\t ||\n");
+        printf("\n\t\t\t||\t3. Aide\t\t ||\n");
+        printf("\n\t\t\t||" ANSI_COLOR_RED "\t0. Quitter\t"ANSI_COLOR_RESET" ||\n" );
+        printf("\n\t\t\tSélectionner une option : ");
         scanf("%d", &choix);
         getchar(); // Pour consommer le '\n' laissé par scanf
 
-        switch (choix)
-        {
-        case 1:
-            system("clear");
-            printf("\nDÉMARRER\n");
-            jeu();
-            break;
+        // Gestion des options du menu
+        switch (choix) {
+            case 1:
+                system("clear");
+                printf("\n\t\t\t"ANSI_COLOR_CYAN"===========================\n");
+                printf("\t\t\t||\tDACTYLOGICIEL\t ||\n");
+                printf("\t\t\t===========================\n\n" ANSI_COLOR_RESET);
+                jeu();
+                break;
 
-        case 2:
-            system("clear");
-            printf("\n\t===========================\n");
-            printf("\t       CONFIGURATION\n");
-            printf("\t===========================\n\n");
-            configurer();
-            break;
+            case 2:
+                system("clear");
+                printf("\n\t\t\t"ANSI_COLOR_CYAN"===========================\n");
+                printf("\t\t\t||\tCONFIGURATION \t ||\n");
+                printf("\t\t\t===========================\n\n" ANSI_COLOR_RESET);
+                configurer();
+                break;
 
-        case 3:
-            system("clear");
+            case 3:
+                system("clear");
+                printf("\n\t\t\t"ANSI_COLOR_CYAN"===========================\n");
+                printf("\t\t\t||\t   AIDE\t\t ||\n");
+                printf("\t\t\t===========================\n\n" ANSI_COLOR_RESET);
+                afficherAide();
+                break;
 
-            printf("\n\t===========================\n");
-            printf("\t               AIDE\n");
-            printf("\t===========================\n\n");
-            afficherAide();
-            break;
+            case 0:
+                system("clear");
+                printf("\n\t\t\t"ANSI_COLOR_RED"------------------------\n");
+                printf("\t\t\tVOUS AVEZ QUITTÉ LE JEU\n");
+                printf("\t\t\t------------------------\n\n" ANSI_COLOR_RESET);
+                printf("\t\t     " ANSI_COLOR_CYAN "APPUYER SUR ENTRER POUR FERMER\n");
+                printf("\t\t\t" ANSI_COLOR_RED "------------------------\n" ANSI_COLOR_RESET);
+                break;
 
-        case 0:
-            system("clear");
-            printf("\n\t\t\t------------------------\n");
-            printf("\t\t\tVOUS AVEZ QUITTÉ LE JEU\n");
-            printf("\t\t\t------------------------\n\n");
-            printf("\t\t     APPUYER SUR ENTRER POUR FERMER\n");
-            printf("\t\t\t------------------------\n");
-            break;
-
-        default:
-            system("clear");
-            printf("\nVeuillez suivre les instructions et choisir une option valide\n");
-            printf("\n_____________________________________________________________\n\n");
-            break;
+            default:
+                system("clear");
+                printf("\n" ANSI_COLOR_RED "Veuillez suivre les instructions et choisir une option valide\n" ANSI_COLOR_RESET);
+                printf("\n_____________________________________________________________\n\n");
+                break;
         }
-    }
-    while (choix != 0);
+    } while (choix != 0);
 
     return 0;
 }
